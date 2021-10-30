8d0d6f8eb44605c91a9d49c08735c1b9bde9dbdc
José Eduardo <josee.edumont@gmail.com>
2021-10-30 19:10:53 -0300

    add early-init.el to improve startup syntax

---
+++ early-init.el
@@ -0,0 +1,4 @@
+(setq gc-cons-threshold (* 20 1024 1024)) ;; initial threshold
+
+(add-hook 'emacs-startup-hook ;; threshold after init
+          (lambda () (setq gc-cons-threshold (* 128 1024 1024))))
